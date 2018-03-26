9f8ae026b8e1891066ce9bac7585c3c6ab7f4997
janeluck <janeluck158@gmail.com>
2018-03-26 15:45:53 +0800

    cookie

--- index.js
+++ index.js
@@ -2,11 +2,45 @@ const Koa = require('koa')
 const Router = require('koa-router')
 const app = new Koa();
 const router = new Router()
-
+const path = require('path')
+const fs = require('fs')
 router.get('/', (ctx, next) => {
-    ctx.body = 'Hello World';
+    let html = `
+
+<script>
+    fetch('/login', {
+        credentials: 'include'
+    }).then(response => {
+
+    })
+</script>
+
+    <ul>
+      <li><a href="/login">login</a></li>
+    </ul>
+  `
+    ctx.body = html
+
 })
+router.get('/login', (ctx, next) => {
+
+
+    ctx.cookies.set(
+        'koaCookie',
+        'xxxxxxx',
+        {
+
+            path: '/',       // 写cookie所在的路径
+            maxAge: 10 * 60 * 1000, // cookie有效时长
+            //  expires: new Date('2017-02-15'),  // cookie失效时间
+            httpOnly: false,  // 是否只用于http请求中获取
+            overwrite: false  // 是否允许重写
+        }
+    )
+    ctx.body = 'cookie is ok'
+})
+
 app.use(router.routes())
     .use(router.allowedMethods())
 
-app.listen(3006);
+app.listen(3009);
